8795db5910795823e9eb0b36d4b7d6a2fee6b1d1
Dan Mills <dgmills914eos@gmail.com>
2020-06-08 18:01:19 -0500

    first components

---
+++ src/components/card/card.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './card.styles.css';
+
+export const Card = props => (
+    <div className='card-containter'>
+        <h1>
+            {props.monster.name}
+        </h1>
+    </div>
+);
+
