0a61ce1b1fef31c9da461178102b849819d425f2
Dan Mills <dgmills914eos@gmail.com>
2020-06-10 19:25:41 -0500

    Updates

---
+++ precache-manifest.3dd3f2cfa8273c26f6f3465d2ff43302.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1483a054b08f18b63dd8d1e9e4a59db2",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "c15d70ae009ec74f8df4",
+    "url": "/monsters-rolodex/static/css/main.8b2f6df1.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c15d70ae009ec74f8df4",
+    "url": "/monsters-rolodex/static/js/main.a790f350.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
